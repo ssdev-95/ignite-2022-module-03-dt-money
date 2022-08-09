2ce1fdd987fc29c19832669695c3ad0f8006816d
Salomao S <souza95salomao@gmail.com>
2022-08-09 00:03:14 +0000

    update: updates some assets/styles and finishes lesson #01

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { ThemeProvider } from 'styled-components'
+
+import { App } from './App'
+
+import { GlobalStyle } from './styles/globals'
+import { defaultTheme } from './styles/themes/defaultTheme'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+	  <ThemeProvider theme={defaultTheme}>
+		  <GlobalStyle />
+      <App />
+		</ThemeProvider>
   </React.StrictMode>
 )
